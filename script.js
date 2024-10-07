820d22f2656902bbe0aacb338e693bc89355ff3e
xeena26 <xeena684346@gmail.com>
2024-10-07 22:33:34 +1100

    Updated CSS and HTML files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any JavaScript effects or interactions here
+    console.log('JavaScript is working!');
 });
