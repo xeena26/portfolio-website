45017af326df1922321336b5bb57a98f315ae5bd
xeena26 <xeena684346@gmail.com>
2024-10-08 20:59:48 +1100

    Added blog page, updates CSS, JS

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any JavaScript effects or interactions here
-    console.log('JavaScript is working!');
-});
+function toggleCard(card) {
+    const fullArticle = card.querySelector('.full-article');
+    const shortDescription = card.querySelector('.short-description');
+    if (fullArticle.classList.contains('d-none')) {
+        fullArticle.classList.remove('d-none');
+        shortDescription.classList.add('d-none');
+    } else {
+        fullArticle.classList.add('d-none');
+        shortDescription.classList.remove('d-none');
+    }
+}
